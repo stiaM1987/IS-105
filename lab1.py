c82213a3b310015d9cf0440a69030f7843cb8b3e
Roleplayingtryhards <freeprojectmind@gmail.com>
2015-02-01 16:46:23 +0100

    Test

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 #
 #  IS-105 LAB1
@@ -8,6 +8,16 @@
 #
 #
 import sys
+import psutil
+import os
+import platform
+import gtk
+
+
+cpu = platform.processor()
+kernel = platform.platform()
+system = platform.system()
+
 
 # Skriv inn fullt navn på gruppemedlemene (erstatte '-' med navn slikt 'Kari Trå')
 gruppe = {  'student1': 'Steffen Vetrhus', 'student2': 'Stian Mønsted'}
@@ -35,7 +45,8 @@ def ascii_bird():
     print   "  \_ /_/"
     print   "  (./"
     print   "   '` "
-    
+
+print "This is assignment 1"    
 print ascii_bird();
 # 
 #  Oppgave 2
@@ -47,8 +58,9 @@ print ascii_bird();
 #					er 4 desimalt. Antagelse: posisjonsbasert tallsystem og 
 #					den mest signifikante bit-en er lengst til venstre
 def bitAnd(x, y):
-	print x & y
+	return x & y;	
 
+print "This is assignment 2"
 print bitAnd(6, 5);
 #
 #  Oppgave 3
@@ -56,8 +68,9 @@ print bitAnd(6, 5);
 #    Eksempel: bitXor(4, 5) = 1
 #
 def bitXor(x, y):
-	print x ^ y
+	return x ^ y
 
+print "This is assignment 3"
 print bitXor(4, 5);
 
 #
@@ -66,8 +79,8 @@ print bitXor(4, 5);
 #    Eksempel: bitOr(0, 1) = 1
 #
 def bitOr(x, y):
-	print x | y
-
+	return x | y
+print "This is assignment 4"
 print bitOr(0, 1);
 
 #
@@ -91,9 +104,15 @@ print bitOr(0, 1);
 #	 Forklar resultatet ascii8Bin('å')
 #	 Hvilke faktorer påvirker resultatet? Forklar.
 #
-def ascii8Bin(letter):
-	pass
 
+#Limitations for this function would be non ascii characters like æøå.
+#The argument value of these are longer one so this test will fail.
+def ascii8Bin(letter):
+	convert = ord(letter)
+	return '{0:08b}'.format(convert)
+
+print "This is assignment 5"
+print ascii8Bin('H');
 # 
 #  Oppgave 6
 #    transferBin - ta en tilfeldig streng som argument og skriver ut en blokk av 8-bits strenger
@@ -105,41 +124,58 @@ def ascii8Bin(letter):
 #	 Skriv selv inn tester ved å bruke assert i funksjonen test()
 #
 def transferBin(string): 
-	l = list(string)
-	for c in l:
+	l = list(string) #L = Liste
+	for c in l: #For each encounter of C in the list L do the following:
 		# skriv ut den binære representasjon av hvert tegn (bruk ascii8Bin funksjonen din)
-		print "Den binære representasjonen for %s" % c
+		print ascii8Bin(c)
+		#print "Den binære representasjonen for %s" % c
 
+
+
+print "This is assignment 6"
+print transferBin("Hi")
 #
 #  Oppgave 7
 #    transferHex - gjør det samme som transferBin, bare skriver ut representasjonen
 #					av strengen heksadesimalt (bruk formattering forklart i Oppgave 6)
-#					Skriv gjerne en støttefunksjon ascii2Hex, som representerer et tegn
+#			b1.py", l		Skriv gjerne en støttefunksjon ascii2Hex, som representerer et tegn
 #					med 2 heksadesimale tegn
 #    Skriv selv inn tester ved å bruke assert i funksjonen test()
 #  
+
+def ascii2hex(bokstav):
+	hexb = ord(bokstav)
+	toHex = '{0:08x}'.format(hexb)
+	return(toHex)
+
 def transferHex(string):
 	l = list(string)
 	for c in l:
+		print ascii2hex(c)		
 		print "Den heksadesimale representasjonen for %s" % c
+
+print "This is assignemnt 7"
+print transferHex("Hi")
 
 #
 # Oppgave 8
 # 		Implementer en funksjon unicodeBin, som kan behandle norske bokstaver
 # 		Kravspesifikasjon for denne funksjonen er den samme som for ascii8Bin funksjonen
 def unicodeBin(character):
-	pass	
+	return '{0:08b}'.format(ord(character))
 
+print "Assignment 8"
+print unicodeBin('å')
 #
 # Oppgave 9
-# 	Studer python module psutils (må være obs på versjon)
+#   Studer python module psutils (må være obs på versjon)
 #   Prøv å finne ut hvordan du kan finne ut og skrive ut følgende informasjon om din 
 #   datamaskin-node:
 #
-# 			Brand and model
+# 			Brand and model //Not
 # 			Hard drive capacity
 # 			Amount of RAM
-# 			Model and speed of CPU
+# 			Model and speed of CPU 
 # 			Display resolution and size
 # 			Operating system
 #	
@@ -147,10 +183,55 @@ def unicodeBin(character):
 #	Skriv en funksjon printSysInfo som skriver ut den informasjon som psutil kan finne.
 #	Kan dere skrive en test for denne funksjonen?
 #	Hvilke andre muligheter har man for å finne informasjon om maskinvare i GNU/Linux?
-#
-def printSysInfo():
-	pass
+#	
+#	cat /etc/system-release
+#	cat /proc/version
+#	cat/etc/lib-release
+#	uname -a
+#	uname -mrs
+#	cat/proc/cpuinfo | grep "model-name"	
+# 	Or import other modules that can find the information you want like
+#	platform
+#	gtk
 
+
+
+def printSysInfo():
+	
+	#Returns all mounted disk partitions
+	diskInfo = psutil.disk_partitions()
+	print diskInfo
+
+	#Returns disk usage statistics about the given path
+	diskUsage = psutil.disk_usage('/')
+	print diskUsage
+
+	#Return statistics about system memory usage
+	memory = psutil.virtual_memory()
+	print memory
+
+	#Return system swap memory statistics
+	swapMemory = psutil.swap_memory()
+	print swapMemory
+	
+	#Return system CPU times
+	cpuTimes = psutil.cpu_times()
+	print cpuTimes
+
+	#Return the logical number of CPUs
+	cpuCount = psutil.cpu_count()
+	print cpuCount
+
+#With the psutil module you can not find information about
+#Brand and Model
+#Model of CPU
+#Display resolution and size
+#Operating system
+
+print "This is assignment 9"
+print printSysInfo();
+print "\nSkjerm info: \n Skjermoppløsning i piksler = %d x %d" % (gtk.gdk.screen_width(),gtk.gdk.screen_height())
+print "\nSystem:\n Prosessor type = %s \n Operativsystem = %s \n Distribusjon = %s" % (cpu, system, kernel)
 
 def test():
 	assert bitAnd(6, 5) == 4
@@ -161,10 +242,10 @@ def test():
 	# Skriv her inn passende tester for tarnsferBin og transferHex funksjoner
 	# fra oppgavene 6 og 7
 	assert unicodeBin('å') == '11100101'
-	# Dine egne tester
+	#Dine egne tester
 	return "Testene er fullført uten feil."
 
 
 # Bruk denne funksjonen for å vise at alle testene er kjørt feilfritt
-#print test()
+print test()
 		
