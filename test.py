7f074ab12046a1dd11412adfa4dc90b0b5f351dc
Roleplayingtryhards <freeprojectmind@gmail.com>
2015-02-23 15:52:36 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 #
 #  IS-105 LAB1
 #
-#  lab1.py - kildekode vil inneholde studentenes løsning.
+#  lab1.py - kildekode vil inneholde studentenes lÃ¸sning.
 #         
 #
 #
@@ -19,17 +19,17 @@ kernel = platform.platform()
 system = platform.system()
 
 
-# Skriv inn fullt navn på gruppemedlemene (erstatte '-' med navn slikt 'Kari Trå')
-gruppe = {  'student1': 'Steffen Vetrhus', 'student2': 'Stian Mønsted'}
+# Skriv inn fullt navn pÃ¥ gruppemedlemene (erstatte '-' med navn slikt 'Kari TrÃ¥')
+gruppe = {  'student1': 'Steffen Vetrhus', 'student2': 'Stian MÃ¸nsted' 'student3': Steven Hicks'}
 
 
 
 #
 #  Oppgave 1
 #    Leke med utskrift 
-#    Skriv ut følgende "ascii art" i en funksjon (erstatte pass)
-#    Funksjonen skal hete ascii_fugl() og skal være uten argumenter og uten returverdier
-#    Den skal skrive ut følgende når den brukes ascii_fugl
+#    Skriv ut fÃ¸lgende "ascii art" i en funksjon (erstatte pass)
+#    Funksjonen skal hete ascii_fugl() og skal vÃ¦re uten argumenter og uten returverdier
+#    Den skal skrive ut fÃ¸lgende nÃ¥r den brukes ascii_fugl
 #
 #       \/_
 #  \,   /( ,/
@@ -51,10 +51,10 @@ print ascii_bird();
 # 
 #  Oppgave 2
 #    bitAnd - x&y
-#	 Implementer funksjonen som gjør en "bitwise" AND operasjon (erstatt pass)
+#	 Implementer funksjonen som gjÃ¸r en "bitwise" AND operasjon (erstatt pass)
 #    Eksempel: bitAnd(6, 5) = 4
-#		Forklaring: 6 binært er 110, mens 5 er 101. Hvis vi sammenligner bitvis
-#					1 AND 1 gir 1, 1 AND 0 gir 0 og 0 AND 1 gir 0 => 100 binært
+#		Forklaring: 6 binÃ¦rt er 110, mens 5 er 101. Hvis vi sammenligner bitvis
+#					1 AND 1 gir 1, 1 AND 0 gir 0 og 0 AND 1 gir 0 => 100 binÃ¦rt
 #					er 4 desimalt. Antagelse: posisjonsbasert tallsystem og 
 #					den mest signifikante bit-en er lengst til venstre
 def bitAnd(x, y):
@@ -87,9 +87,9 @@ print bitOr(0, 1);
 #  Oppgave 5
 #
 #  Tips:
-#    For å finne desimalverdien til et tegn kan funksjonen ord brukes, for eksempel
+#    For Ã¥ finne desimalverdien til et tegn kan funksjonen ord brukes, for eksempel
 #      ord('A') , det vil gi et tall 65 i ti-tallssystemet
-#    For å formattere 6 i ti-tallssystemet til 00000110 i to-tallssystemet
+#    For Ã¥ formattere 6 i ti-tallssystemet til 00000110 i to-tallssystemet
 #      '{0:08b}'.format(6)
 #      00000110
 #
@@ -97,15 +97,15 @@ print bitOr(0, 1);
 #      {} setter en variabel inn i strengen
 #      0 tar variabelen i argument posisjon 0
 #      : legger til formatteringsmuligheter for denne variabelen (ellers hadde den 6 desimalt)
-#      08 formatterer tall til 8 tegn og fuller med nuller til venstre hvis nødvendig
-#      b konverterer tallet til dets binære representasjon
+#      08 formatterer tall til 8 tegn og fuller med nuller til venstre hvis nÃ¸dvendig
+#      b konverterer tallet til dets binÃ¦re representasjon
 #
-#	 Hvilke begrensninger vil en slik funksjon ha? (tips: prøv med bokstaven 'å', f.eks.)
-#	 Forklar resultatet ascii8Bin('å')
-#	 Hvilke faktorer påvirker resultatet? Forklar.
+#	 Hvilke begrensninger vil en slik funksjon ha? (tips: prÃ¸v med bokstaven 'Ã¥', f.eks.)
+#	 Forklar resultatet ascii8Bin('Ã¥')
+#	 Hvilke faktorer pÃ¥virker resultatet? Forklar.
 #
 
-#Limitations for this function would be non ascii characters like æøå.
+#Limitations for this function would be non ascii characters like Ã¦Ã¸Ã¥.
 #The argument value of these are longer one so this test will fail.
 def ascii8Bin(letter):
 	convert = ord(letter)
@@ -116,19 +116,19 @@ print ascii8Bin('H');
 # 
 #  Oppgave 6
 #    transferBin - ta en tilfeldig streng som argument og skriver ut en blokk av 8-bits strenger
-#                  som er den binære representasjon av strengen
+#                  som er den binÃ¦re representasjon av strengen
 #    Eksempel: transferBin("Hi") skriver ut: 
 #                01001000
 #                01101001
-#	 Forklart hver linje i denne funksjonen (hva er list, hva gjør in)
-#	 Skriv selv inn tester ved å bruke assert i funksjonen test()
+#	 Forklart hver linje i denne funksjonen (hva er list, hva gjÃ¸r in)
+#	 Skriv selv inn tester ved Ã¥ bruke assert i funksjonen test()
 #
 def transferBin(string): 
 	l = list(string) #L = Liste
 	for c in l: #For each encounter of C in the list L do the following:
-		# skriv ut den binære representasjon av hvert tegn (bruk ascii8Bin funksjonen din)
+		# skriv ut den binÃ¦re representasjon av hvert tegn (bruk ascii8Bin funksjonen din)
 		print ascii8Bin(c)
-		#print "Den binære representasjonen for %s" % c
+		#print "Den binÃ¦re representasjonen for %s" % c
 
 
 
@@ -136,11 +136,11 @@ print "This is assignment 6"
 print transferBin("Hi")
 #
 #  Oppgave 7
-#    transferHex - gjør det samme som transferBin, bare skriver ut representasjonen
+#    transferHex - gjÃ¸r det samme som transferBin, bare skriver ut representasjonen
 #					av strengen heksadesimalt (bruk formattering forklart i Oppgave 6)
-#			b1.py", l		Skriv gjerne en støttefunksjon ascii2Hex, som representerer et tegn
+#			b1.py", l		Skriv gjerne en stÃ¸ttefunksjon ascii2Hex, som representerer et tegn
 #					med 2 heksadesimale tegn
-#    Skriv selv inn tester ved å bruke assert i funksjonen test()
+#    Skriv selv inn tester ved Ã¥ bruke assert i funksjonen test()
 #  
 
 def ascii2hex(bokstav):
@@ -165,11 +165,11 @@ def unicodeBin(character):
 	return '{0:08b}'.format(ord(character))
 
 print "Assignment 8"
-print unicodeBin('å')
+print unicodeBin('Ã¥')
 #
 # Oppgave 9
-#   Studer python module psutils (må være obs på versjon)
-#   Prøv å finne ut hvordan du kan finne ut og skrive ut følgende informasjon om din 
+#   Studer python module psutils (mÃ¥ vÃ¦re obs pÃ¥ versjon)
+#   PrÃ¸v Ã¥ finne ut hvordan du kan finne ut og skrive ut fÃ¸lgende informasjon om din 
 #   datamaskin-node:
 #
 # 			Brand and model //Not
@@ -182,7 +182,7 @@ print unicodeBin('å')
 #	Forklar hvorfor man kan / ikke kan finne denne informasjon vha. psutil modulen.
 #	Skriv en funksjon printSysInfo som skriver ut den informasjon som psutil kan finne.
 #	Kan dere skrive en test for denne funksjonen?
-#	Hvilke andre muligheter har man for å finne informasjon om maskinvare i GNU/Linux?
+#	Hvilke andre muligheter har man for Ã¥ finne informasjon om maskinvare i GNU/Linux?
 #	
 #	cat /etc/system-release
 #	cat /proc/version
@@ -230,7 +230,7 @@ def printSysInfo():
 
 print "This is assignment 9"
 print printSysInfo();
-print "\nSkjerm info: \n Skjermoppløsning i piksler = %d x %d" % (gtk.gdk.screen_width(),gtk.gdk.screen_height())
+print "\nSkjerm info: \n SkjermopplÃ¸sning i piksler = %d x %d" % (gtk.gdk.screen_width(),gtk.gdk.screen_height())
 print "\nSystem:\n Prosessor type = %s \n Operativsystem = %s \n Distribusjon = %s" % (cpu, system, kernel)
 
 def test():
@@ -241,11 +241,11 @@ def test():
 	assert ascii8Bin('A') == '01000001'
 	# Skriv her inn passende tester for tarnsferBin og transferHex funksjoner
 	# fra oppgavene 6 og 7
-	assert unicodeBin('å') == '11100101'
+	assert unicodeBin('Ã¥') == '11100101'
 	#Dine egne tester
-	return "Testene er fullført uten feil."
+	return "Testene er fullfÃ¸rt uten feil."
 
 
-# Bruk denne funksjonen for å vise at alle testene er kjørt feilfritt
+# Bruk denne funksjonen for Ã¥ vise at alle testene er kjÃ¸rt feilfritt
 print test()
 		
