629901052d181fe426a1b1baba9570f55601d94a
Roleplayingtryhards <freeprojectmind@gmail.com>
2015-02-23 16:10:46 +0100

    Delete UDPClient.py
    Flyttet til mappe

--- UDPClient.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-serverName = 'hostname'
-serverPort = 12000
-clientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
-message = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message,(serverName, serverport))
-modifiedMEssage, serverAddress = clientSocket.recvfrom(2048)
-print modifiedMessage
-clientSocket.close()
